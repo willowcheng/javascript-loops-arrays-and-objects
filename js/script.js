2b4ed559f6777613cc17037cc4c49550198325a0
willowcheng <e.wingus@Gmail.com>
2015-04-08 14:22:06 -0400

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-var randomNumber = getRandomNumber(10);
-var guess;
-var guessCount = 0;
-var correctGuess = false;
+var html = '';
+var rgbColor;
 
-function getRandomNumber(upper) {
-    var num = Math.floor(Math.random() * upper) + 1;
-    return num;
+function randomRGB() {
+    return Math.floor(Math.random() * 256);
 }
 
-while (guessCount < 10) {
-    guess = prompt('I am thinking of a number between 1 and 10. What is it?');
-    guessCount += 1;
-    if (parseInt(guess) === randomNumber) {
-        correctGuess = true;
-        break;
-    }
+function randomColor() {
+    var color = 'rgb(';
+    color += randomRGB() + ',';
+    color += randomRGB() + ',';
+    color += randomRGB() + ')';
+    return color;
 }
 
-if (correctGuess) {
-    document.write('<h1>You guessed the number!</h1>');
-    document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
-} else {
-    document.write('<h1>Sorry. You did not guess the number.</h1>');
+function print(message) {
+    document.write(message);
 }
+
+for (var dummy = 0; dummy < 10; dummy++){
+    rgbColor = randomColor()
+    html += '<div style="background-color:' + rgbColor + '"></div>';
+}
+
+print(html);
