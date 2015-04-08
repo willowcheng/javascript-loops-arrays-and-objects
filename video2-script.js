559c722775be7f70155185ba3d87ede30eee77e8
willowcheng <e.wingus@Gmail.com>
2015-04-08 00:27:43 -0400

    A closer look at loop conditions

--- video2-script.js
+++ video2-script.js
@@ -1,8 +1,8 @@
 function randomNumber(upper) {
-    return Math.floor(Math.random() * upper) + 1;
+    return Math.floor( Math.random() * upper ) + 1;
 }
 var counter = 0;
-while (counter < 100) {
+while ( counter < 10000 ) {
     var randNum = randomNumber(6);
     document.write(randNum + ' ');
     counter += 1;
