105553bce47adbd3229dfee1d13c5c8bc33ef5ed
willowcheng <e.wingus@Gmail.com>
2015-04-08 13:55:38 -0400

    For loop

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
-var randomNumber = getRandomNumber(10);
-var guess;
-var guessCount = 0;
-var correctGuess = false;
+var html = '';
 
-function getRandomNumber(upper) {
-    var num = Math.floor(Math.random() * upper) + 1;
-    return num;
+for (var i = 1; i <= 100; i++) {
+    html += '<div>' + i + '</div>';
 }
 
-do {
-    guess = prompt('I am thinking of a number between 1 and 10. What is it?');
-    guessCount += 1;
-    if (parseInt(guess) === randomNumber) {
-        correctGuess = true;
-    }
-} while (!correctGuess);
-document.write('<h1>You guessed the number!</h1>');
-document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+document.write(html);
