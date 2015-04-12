9d26dc84adf9f098a16fa7b06eb6aba94e1fbad6
willowcheng <e.wingus@Gmail.com>
2015-04-12 11:05:00 -0400

    Accessing items in an array

---
+++ js/shopping.js
@@ -0,0 +1,9 @@
+var shoppingList = [
+    'carrots',
+    'milk',
+    'eggs'
+];
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
+console.log(shoppingList[2]);
+console.log(shoppingList[3]);
