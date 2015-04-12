0011260f8347182314891bb0274141295a5f8409
willowcheng <e.wingus@Gmail.com>
2015-04-12 11:17:54 -0400

    Adding data to arrays

---
+++ js/playlist.js
@@ -0,0 +1,7 @@
+var playList = [];
+playList.push('I Did It My Way');
+playList.push('Respect', 'Imagine');
+playList.unshift('Burn to Run');
+playList.unshift('Louie Louie', 'Maybellene');
+
+printList(playList);
--- js/shopping.js
+++
@@ -1,9 +0,0 @@
-var shoppingList = [
-    'carrots',
-    'milk',
-    'eggs'
-];
-console.log(shoppingList[0]);
-console.log(shoppingList[1]);
-console.log(shoppingList[2]);
-console.log(shoppingList[3]);
