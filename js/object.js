8f1d69ff16911764dd0d270abaa83528acad051b
willowcheng <e.wingus@Gmail.com>
2015-04-21 08:07:13 -0400

    Using 'for in' to loop through an object's properties

--- js/object.js
+++ js/object.js
@@ -6,17 +6,6 @@ var person = {
     skills : ['JavaScript', 'HTML', 'CSS']
 };
 
-function print(message) {
-    var div = document.getElementById('output');
-    div.innerHTML = message;
+for (var prop in person) {
+    console.log(prop, ': ', person[prop]);
 }
-
-var message = '<p>Hello. My name is ' + person.name + '</p>';
-message += '<p>I live in the ' + person.country + '</p>';
-person.name = 'Rainbow Dash';
-message += '<p>But, I wish my name was ' + person.name + '</p>';
-person.age++;
-message += '<p>My age is now ' + person.age + '</p>';
-message += '<p>I have ' + person.skills.length + ' skills: ';
-message += person.skills.join(', ') + '</p>';
-print(message);
